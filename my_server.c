81552a84619657f0fe2a8d46158a8f1f5d0a8cfe
Micaela Banfi <mbanfi@itba.edu.ar>
2019-06-15 17:40:14 -0300

    add \

--- my_server.c
+++ my_server.c
@@ -41,8 +41,12 @@ char * desafios[11] = {
      ".data .bss .comment ? .shstrtab .symtab .strtab",
      "mixed fds",
      "Tango Hotel India Sierra India Sierra Alfa Whiskey Echo Sierra Oscar Mike Echo",
-     "quine \\ gcc: error: quine.c: No such file or directory \\ gcc: fatal error: no input files \\ compilation terminated",
-      "b gbdme y encontrará el valor mágico \\  try again",
+     "quine \
+     gcc: error: quine.c: No such file  directory \
+     gcc: fatal error: no input files \
+    compilation terminated",
+      "b gbdme y encontrará el valor mágico \
+      try again",
       "/lib/x86_64-linux-gnu/libc-2.19.s0 ?",
       
 
